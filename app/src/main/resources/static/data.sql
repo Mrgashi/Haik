ce37eea2278caf9100c967fb6bd33faff64f020f
Line Kirkhus <kirkhusline@gmail.com>
2020-03-17 14:25:49 +0100

    Removed file

--- app/src/main/resources/static/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Haik.User (id, firstname, lastname, email, password, phone_number) VALUES (1, 'Mirdon', 'Gashi', 'mirdon_g@hotmail.com', '123', '12345678');
-INSERT INTO Haik.ride (created, startdate, seatsavailable, startlocation, destination, comment) VALUES (02022020, 0202020, 2, 'Larvik', 'Oslo', 'Stopper inne IKEA!');
